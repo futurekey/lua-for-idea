f65f9c0a80fe437c90e4b42cd4247bddd8af9d2c
sylvanaar <sylvanaar@mindspring.com>
2012-07-03 04:31:57 -0400

    start idea12 branch

---
+++ src/lang/psi/stubs/LuaStubUtils.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2011 Jon S Akhtar (Sylvanaar)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.stubs;
+
+import com.intellij.psi.stubs.*;
+import com.sylvanaar.idea.Lua.lang.psi.expressions.*;
+import com.sylvanaar.idea.Lua.lang.psi.stubs.api.*;
+import com.sylvanaar.idea.Lua.lang.psi.types.*;
+import org.jetbrains.annotations.*;
+
+import java.io.*;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 02.06.2009
+ */
+public class LuaStubUtils {
+    public static void writeNullableString(StubOutputStream dataStream, @Nullable String typeText) throws IOException {
+        dataStream.writeBoolean(typeText != null);
+        if (typeText != null) {
+            dataStream.writeUTFFast(typeText);
+        }
+    }
+
+    @Nullable
+    public static String readNullableString(StubInputStream dataStream) throws IOException {
+        final boolean hasTypeText = dataStream.readBoolean();
+        return hasTypeText ? dataStream.readUTFFast() : null;
+    }
+
+    public static void writePrimativeTypeOrLength(LuaType type, byte[] encoded,
+                                                  StubOutputStream dataStream) throws IOException {
+        if (type instanceof LuaPrimativeType) {
+            dataStream.writeShort(((LuaPrimativeType) type).getId());
+        } else {
+            dataStream.writeShort(encoded.length);
+            dataStream.write(encoded);
+        }
+    }
+
+    public static LuaType readePrimativeType(StubInputStream dataStream, int len) throws IOException {
+        if (len < 0) SerializationManager.getInstance().repairNameStorage();
+        final LuaType[] types = LuaPrimativeType.PRIMATIVE_TYPES;
+        if (len >= types.length)
+            return null;
+
+        return types[len];
+    }
+
+    public static byte[] readEncodedType(StubInputStream dataStream, int len) throws IOException {
+        byte[] typedata = new byte[len];
+        dataStream.read(typedata, 0, len);
+        return typedata;
+    }
+
+    public static LuaType GetStubOrPrimativeType(LuaTypedStub stub, LuaExpression psi) {
+        final LuaType luaType = stub.getLuaType();
+        return luaType != null ? luaType : new StubType(stub.getEncodedType());
+    }
+
+    public static class ExtractVariableType {
+        private StubInputStream dataStream;
+        private LuaType         type;
+        private byte[]          typedata;
+
+        public ExtractVariableType(StubInputStream dataStream) {this.dataStream = dataStream;}
+
+        public LuaType getType() {
+            return type;
+        }
+
+        public byte[] getTypedata() {
+            return typedata;
+        }
+
+        public ExtractVariableType invoke() throws IOException {
+            int len = dataStream.readShort();
+            type = readePrimativeType(dataStream, len);
+            typedata = null;
+            if (type != null) typedata = readEncodedType(dataStream, len);
+            return this;
+        }
+    }
+}
---
+++ src/lang/psi/stubs/elements/LuaStubCompoundIdentifierType.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2011 Jon S Akhtar (Sylvanaar)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.sylvanaar.idea.Lua.lang.psi.stubs.elements;
+
+import com.intellij.psi.stubs.*;
+import com.intellij.util.io.*;
+import com.sylvanaar.idea.Lua.lang.psi.impl.symbols.*;
+import com.sylvanaar.idea.Lua.lang.psi.stubs.*;
+import com.sylvanaar.idea.Lua.lang.psi.stubs.api.*;
+import com.sylvanaar.idea.Lua.lang.psi.stubs.impl.*;
+import com.sylvanaar.idea.Lua.lang.psi.stubs.index.*;
+import com.sylvanaar.idea.Lua.lang.psi.symbols.*;
+import com.sylvanaar.idea.Lua.lang.psi.types.*;
+
+import java.io.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jon S Akhtar
+ * Date: 2/21/11
+ * Time: 7:32 PM
+ */
+public class LuaStubCompoundIdentifierType
+    extends LuaStubElementType<LuaCompoundIdentifierStub, LuaCompoundIdentifier> {
+    public LuaStubCompoundIdentifierType() {
+        super("compound id stub name");
+    }
+
+    @Override
+    public LuaCompoundIdentifier createPsi(LuaCompoundIdentifierStub stub) {
+        return new LuaCompoundIdentifierImpl(stub);
+    }
+
+    @Override
+    public LuaCompoundIdentifierStub createStub(LuaCompoundIdentifier psi, StubElement parentStub) {
+        return new LuaCompoundIdentifierStubImpl(parentStub, psi);
+    }
+
+    @Override
+    public String getExternalId() {
+        return "lua.COMPOUND_ID";
+    }
+
+    @Override
+    public void serialize(LuaCompoundIdentifierStub stub, StubOutputStream dataStream) throws IOException {
+        dataStream.writeName(stub.getName());
+        LuaStubUtils.writePrimativeTypeOrLength(stub.getLuaType(), stub.getEncodedType(), dataStream);
+        dataStream.writeBoolean(stub.isGlobalDeclaration());
+    }
+
+    @Override
+    public LuaCompoundIdentifierStub deserialize(StubInputStream dataStream, StubElement parentStub) throws IOException {
+        StringRef ref = dataStream.readName();
+
+        assert ref != null : "Null name in stub stream";
+
+        LuaStubUtils.ExtractVariableType extractVariableType = new LuaStubUtils.ExtractVariableType(dataStream).invoke();
+        byte[] typedata = extractVariableType.getTypedata();
+        LuaType type = extractVariableType.getType();
+
+        boolean isDeclaration = dataStream.readBoolean();
+
+        return new LuaCompoundIdentifierStubImpl(parentStub, ref, isDeclaration, typedata, type);
+    }
+
+    @Override
+    public void indexStub(LuaCompoundIdentifierStub stub, IndexSink sink) {
+        String name = stub.getName();
+
+        if (name != null && stub.isGlobalDeclaration()) {
+          sink.occurrence(LuaGlobalDeclarationIndex.KEY, name);
+        }
+    }
+}
